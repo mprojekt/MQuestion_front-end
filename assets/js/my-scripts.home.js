6567746820c5eacad866c0c3a61d70a67da4a4d6
marcin <mpietroszek@gmail.com>
2015-12-07 20:17:01 +0100

    >index< page start

---
+++ assets/js/my-scripts.home.js
@@ -0,0 +1,13 @@
+$(function () {
+	// start config:
+
+	// jqueru ui draggable
+	$('.draggable').draggable({ 
+		handle: ".panel-heading", 
+		containment: "body", 
+		zIndex: 100 
+	});
+	
+	
+});
+
