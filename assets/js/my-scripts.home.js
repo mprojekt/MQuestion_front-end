b0672a81602a9bbbba325360e104863752e2f6c6
marcin <mpietroszek@gmail.com>
2015-12-07 23:54:32 +0100

    add >index< page and some fixes

--- assets/js/my-scripts.home.js
+++ assets/js/my-scripts.home.js
@@ -1,13 +1,20 @@
 $(function () {
-	// start config:
-
+	
+	// for tests!
+	var questionTmp = $('.question').last();	
+	for(i = 1; i < 5; i++){
+		var T = questionTmp.position().top - 20 - (i*100);
+		var L = questionTmp.position().left - 15 + (i*50);
+		$('.question').last().after($('.question').first().clone().css({'posiotion':'relativ', 'top':T, 'left':L}));
+		$('.result').last().after($('.result').first().clone().css({'posiotion':'relativ', 'top':T, 'left':L}));
+	}
+	
 	// jqueru ui draggable
 	$('.draggable').draggable({ 
 		handle: ".panel-heading", 
 		containment: "body", 
 		zIndex: 100 
 	});
-	
 	
 });
 
